d3f5094275dbbf3a45a9ed8c348a975024898aab
jmsifsc <joel.ms@aluno.ifsc.edu.br>
2025-06-24 16:11:26 -0300

    modificacoes

--- conexao.php
+++ conexao.php
@@ -4,6 +4,13 @@ $host = "localhost";
 $db = "loja";
 $user = "root";
 $pass = "";
+
+try{
+    $pdo = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8',$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+        die("Erro ao conectar".$e->getMessage());
+    }
 $conexao = mysqli_connect($host, $user, $pass, $db);
 
 if($conexao->connect_error) {
