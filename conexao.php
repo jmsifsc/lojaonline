ce81aa2d0dd1012bd79388fb30fd2ab4562e74e4
jmsifsc <joel.ms@aluno.ifsc.edu.br>
2025-06-24 17:12:24 -0300

    modificacoes conexao

--- conexao.php
+++ conexao.php
@@ -11,9 +11,4 @@ try{
     }catch(PDOException $e){
         die("Erro ao conectar".$e->getMessage());
     }
-$conexao = mysqli_connect($host, $user, $pass, $db);
-
-if($conexao->connect_error) {
-    die("Erro ao conectar" . $conexao->connect_error);
-}
 ?>
